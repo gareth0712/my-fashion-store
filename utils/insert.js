a50ff52b2d5066d0114b750ab2e29ff4f1a4a19a
gareth0712 <gareth_0712@hotmail.com>
2021-02-10 11:42:42 +0800

    Update insert.js for handling errors

--- utils/insert.js
+++ utils/insert.js
@@ -8,19 +8,27 @@ const performanceMetrics = require('./performanceMetrics');
 
 const salesRecordsStream = (path) => {
   let salesRecordsCount = 0;
+  let encounteredError = false;
   const results = [];
   performanceMetrics.start();
 
   // Make use of createReadStream for handling very big files
-  return new Promise((resolve, reject) =>
-    fs
-      .createReadStream(path)
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(path)
       .pipe(es.split()) // Split each line of the file
       .pipe(
         es.mapSync(async (line) => {
           // Skip first ilne or any empty line
-          if (line.includes('USER_NAME') || line === '') return;
-          results.push(csvParser(line));
+          if (line.includes('USER_NAME') || line === '' || encounteredError)
+            return;
+          try {
+            // salesRecordCount + 1 as the nth record
+            results.push(csvParser(line, salesRecordsCount + 1));
+            salesRecordsCount += 1;
+          } catch (e) {
+            encounteredError = true;
+            reject(e);
+          }
           // set the chunked data to size of 50000 each
           if (results.length >= 50000) {
             const resultsToInsert = [...results];
@@ -31,13 +39,13 @@ const salesRecordsStream = (path) => {
             // Reset duplicated results array as well
             resultsToInsert.length = 0;
           }
-          salesRecordsCount += 1;
         })
       )
-      .on('error', (e) => reject(e))
       .on('end', async () => {
+        if (encounteredError) return;
         // For adding last chunk of data of size below 50,000
         if (results.length > 0) await Sales.insertMany(results);
+        results.length = 0;
         if (process.env.NODE_ENV === 'development') {
           console.log(salesRecordsCount);
           // Performance metrics show runtime and memory used
@@ -47,12 +55,12 @@ const salesRecordsStream = (path) => {
           );
         }
         resolve(salesRecordsCount);
-      })
-  );
+      });
+  });
 };
 
 module.exports = async (path) => {
-  // Return error if there is problem with csv file
+  // Throw error if there is problem with csv file
   await validateCsvFile(path);
   return await salesRecordsStream(path);
 };
