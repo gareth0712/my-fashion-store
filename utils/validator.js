024672680787b620ccb0c5889fc3d6accb537a3f
gareth0712 <gareth_0712@hotmail.com>
2021-02-10 11:43:18 +0800

    refractor validateGender for readibility

--- utils/validator.js
+++ utils/validator.js
@@ -11,21 +11,19 @@ const validateCsvFile = async (path) => {
 };
 
 const validateGender = (gender, index) => {
-  if (['M', 'F'].includes(gender) || gender === '') return gender;
+  if (['M', 'F', ''].includes(gender)) return gender;
   if (['m', 'f'].includes(gender)) return gender.toUpperCase();
-  const message = `Please provide valid input for GENDER ('M' or 'F') for record #${
-    index + 1
-  }`;
-  throw new AppError(message, 400);
+  const message = `Please provide valid input for GENDER ('M' or 'F') for record #${index}`;
+  return new AppError(message, 400);
 };
 
 const validateNumber = (field, value, index, max = 0) => {
   let message;
   if (value === '') return value;
   if (value <= 0) {
-    message = `${field} should not be negative number for record #${index + 1}`;
+    message = `${field} should not be negative number for record #${index}`;
   } else if (max > 0 && value > max) {
-    message = `${field} should not exceed ${max} for record #${index + 1}`;
+    message = `${field} should not exceed ${max} for record #${index}`;
   }
   if (message) throw new AppError(message, 400);
   return value;
@@ -34,15 +32,13 @@ const validateNumber = (field, value, index, max = 0) => {
 const validateLastPurchaseDate = (date, index) => {
   if (date === '')
     throw new AppError(
-      `LAST_PURCHASE_DATE should not be empty for record #${index + 1}`,
+      `LAST_PURCHASE_DATE should not be empty for record #${index}`,
       400
     );
   const parsedDate = Date.parse(date);
   if (Number.isNaN(parsedDate) || parsedDate < 0 || parsedDate > Date.now())
     throw new AppError(
-      `Please provide a valid date string in LAST_PURCHASE_DATE field for record ${
-        index + 1
-      }`,
+      `Please provide a valid date string in LAST_PURCHASE_DATE field for record ${index}`,
       400
     );
   return new Date(date).toISOString();
