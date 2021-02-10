2d3b1609efc05aad824e8140810c84a980cc9267
gareth0712 <gareth_0712@hotmail.com>
2021-02-10 09:15:42 +0800

    Update insert and csvParser for handling very big files

--- utils/csvParser.js
+++ utils/csvParser.js
@@ -1,43 +1,17 @@
-const fs = require('fs').promises;
-
 const {
   validateGender,
   validateNumber,
   validateLastPurchaseDate,
 } = require('./validator');
-const performanceMetrics = require('./performanceMetrics');
 
-performanceMetrics.start();
-
-module.exports = async (filePath) => {
-  const data = await fs.readFile(filePath, 'binary');
-  const jsonArray = [];
-  const arr = data.split('\r\n');
-
-  // Remove header
-  arr.shift();
-  // Remove last empty row if any
-  while (arr[arr.length - 1] === '') {
-    arr.pop();
-  }
-
-  arr.forEach((el, i) => {
-    const tempJson = {};
-    const temp = el.split(',');
-    tempJson.userName = temp[0].trim();
-    tempJson.age = validateNumber('AGE', temp[1], i, 130);
-    tempJson.height = validateNumber('HEIGHT', temp[2], i, 300);
-    tempJson.gender = validateGender(temp[3], i);
-    tempJson.saleAmount = validateNumber('SALE_AMOUNT', temp[4], i);
-    tempJson.lastPurchaseDate = validateLastPurchaseDate(temp[5], i);
-
-    jsonArray.push(tempJson);
-  });
-  if (process.env.NODE_ENV === 'development') {
-    console.log(
-      'Performance metrics of Parse -',
-      performanceMetrics.showPerformanceData()
-    );
-  }
-  return jsonArray;
+module.exports = (csvString, i) => {
+  const json = {};
+  const csvArray = csvString.split(',');
+  json.userName = csvArray[0].trim();
+  json.age = validateNumber('AGE', csvArray[1], i, 130);
+  json.height = validateNumber('HEIGHT', csvArray[2], i, 300);
+  json.gender = validateGender(csvArray[3], i);
+  json.saleAmount = validateNumber('SALE_AMOUNT', csvArray[4], i);
+  json.lastPurchaseDate = validateLastPurchaseDate(csvArray[5], i);
+  return json;
 };
--- utils/insert.js
+++ utils/insert.js
@@ -1,20 +1,58 @@
+const fs = require('fs');
+const es = require('event-stream');
+
 const csvParser = require('./csvParser');
 const Sales = require('../models/salesModel');
 const { validateCsvFile } = require('./validator');
 const performanceMetrics = require('./performanceMetrics');
 
-module.exports = async (path) => {
-  await validateCsvFile(path);
+const salesRecordsStream = (path) => {
   let salesRecordsCount = 0;
+  const results = [];
   performanceMetrics.start();
-  const data = await csvParser(path);
-  await Sales.insertMany(data);
-  salesRecordsCount += data.length;
-  if (process.env.NODE_ENV === 'development') {
-    console.log(
-      'Performance metrics of Insert -',
-      performanceMetrics.showPerformanceData()
-    );
-  }
-  return salesRecordsCount;
+
+  // Make use of createReadStream for handling very big files
+  return new Promise((resolve, reject) =>
+    fs
+      .createReadStream(path)
+      .pipe(es.split()) // Split each line of the file
+      .pipe(
+        es.mapSync(async (line) => {
+          // Skip first ilne or any empty line
+          if (line.includes('USER_NAME') || line === '') return;
+          results.push(csvParser(line));
+          // set the chunked data to size of 50000 each
+          if (results.length >= 50000) {
+            const resultsToInsert = [...results];
+            // Reset results array for memory concern
+            results.length = 0;
+            // Insert results by chunk of destinated size
+            await Sales.insertMany(resultsToInsert);
+            // Reset duplicated results array as well
+            resultsToInsert.length = 0;
+          }
+          salesRecordsCount += 1;
+        })
+      )
+      .on('error', (e) => reject(e))
+      .on('end', async () => {
+        // For adding last chunk of data of size below 50,000
+        if (results.length > 0) await Sales.insertMany(results);
+        if (process.env.NODE_ENV === 'development') {
+          console.log(salesRecordsCount);
+          // Performance metrics show runtime and memory used
+          console.log(
+            'Performance metrics:',
+            performanceMetrics.showPerformanceData()
+          );
+        }
+        resolve(salesRecordsCount);
+      })
+  );
+};
+
+module.exports = async (path) => {
+  // Return error if there is problem with csv file
+  await validateCsvFile(path);
+  return await salesRecordsStream(path);
 };
